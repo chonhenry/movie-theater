c1ba8c8c8a3d8d00c98948d16e915587745d2824
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-04-26 00:58:28 -0400

    add style to navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,21 +5,41 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="navbar ui menu">
-      <Link to="/" className="logo">
-        <img src={logo} alt="logo"></img>
-      </Link>
-      <Link to="/" className="link">
-        HOME
-      </Link>
-      <Link to="/location" className="link">
-        THEATERS
-      </Link>
-      <Link to="/location" className="link">
-        SEARCH
-      </Link>
-    </div>
+    <nav className="nav-wrapper">
+      <div className="container">
+        <ul id="nav-mobile" className="">
+          <li>
+            <Link to="/" className="logo brand-logo">
+              <img src={logo} alt="logo"></img>
+            </Link>
+          </li>
+          <li>
+            <Link to="/" className="link">
+              HOME
+            </Link>
+          </li>
+          <li>
+            <Link to="/location" className="link">
+              THEATERS
+            </Link>
+          </li>
+          <li>
+            <Link to="/location" className="link">
+              SEARCH
+            </Link>
+          </li>
+        </ul>
+
+        <div class="right">
+          <Link to="/location" className="link">
+            PROFILE
+          </Link>
+        </div>
+      </div>
+    </nav>
   );
 };
 
 export default Navbar;
+
+// https://materializecss.com/navbar.html
