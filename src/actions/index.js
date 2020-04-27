e48afa7a2354d044259f6d91f3a419d617c430bb
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-04-27 18:22:38 -0400

    add redux

---
+++ src/actions/index.js
@@ -0,0 +1,5 @@
+export const fetchNowPlayingMovie = () => {
+  return {
+    type: "Fetch_Now_Playing_Movie",
+  };
+};
--- src/pages/homepage/homepage.js
+++ src/pages/homepage/homepage.js
@@ -2,25 +2,35 @@ import React from "react";
 import BoxContainer from "../../components/box-container/box-container";
 import Box from "../../components/box/box";
 import MovieItem from "../../components/movie-item/movie-item";
+import { connect } from "react-redux";
+import { fetchNowPlayingMovie } from "../../actions/index";
 import "./homepage.scss";
 
-const Homepage = (props) => {
-  return (
-    <div className="homepage container">
-      <BoxContainer>
-        <Box linkTo="/">Latest</Box>
-        <Box linkTo="/">Now Playing</Box>
-        <Box linkTo="/">Top Rated</Box>
-        <Box linkTo="/">Upcoming</Box>
-      </BoxContainer>
-      <h1>Now Playing</h1>
-      <BoxContainer>
-        <Box linkTo="/">Upcoming</Box>
-        <Box linkTo="/">Upcoming</Box>
-      </BoxContainer>
-      <MovieItem movieItem={props.movieItem}>{props.movieItem}</MovieItem>
-    </div>
-  );
-};
+class Homepage extends React.Component {
+  componentDidMount() {
+    this.props.fetchNowPlayingMovie();
+  }
 
-export default Homepage;
+  render() {
+    return (
+      <div className="homepage container">
+        <BoxContainer>
+          <Box linkTo="/">Latest</Box>
+          <Box linkTo="/">Now Playing</Box>
+          <Box linkTo="/">Top Rated</Box>
+          <Box linkTo="/">Upcoming</Box>
+        </BoxContainer>
+        <h1>Now Playing</h1>
+        <BoxContainer>
+          <Box linkTo="/">Upcoming</Box>
+          <Box linkTo="/">Upcoming</Box>
+        </BoxContainer>
+        <MovieItem movieItem={this.props.movieItem}>
+          {this.props.movieItem}
+        </MovieItem>
+      </div>
+    );
+  }
+}
+
+export default connect(null, { fetchNowPlayingMovie })(Homepage);
