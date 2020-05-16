f877adae96e2967081a6ed2f4b9778cdbba3e55c
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-05-16 19:29:40 -0400

    add style to payment form

--- src/pages/payment/payment.js
+++ src/pages/payment/payment.js
@@ -11,44 +11,49 @@ class Payment extends React.Component {
           <div className="title">title: this.props.title</div>
           <div className="payment-detail">
             <div className="credit-card-form">
-              <div>Enter you payment details</div>
+              <div className="payment-label">Enter you payment details</div>
 
-              <div>Cardholder Name</div>
-              <input type="text"></input>
+              <div className="label">Cardholder Name</div>
+              <input type="text" className="cardholder-name"></input>
 
-              <div>Card Number</div>
-              <input type="text"></input>
+              <div className="label">Card Number</div>
+              <input type="text" className="card-number"></input>
 
-              <div>
-                <div className="exp-date">Exp Month</div>
-                <select>
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                  <option>4</option>
-                  <option>5</option>
-                  <option>6</option>
-                  <option>7</option>
-                  <option>8</option>
-                  <option>9</option>
-                  <option>10</option>
-                  <option>11</option>
-                  <option>12</option>
-                </select>
+              <div className="expiration">
+                <div className="exp-month-container">
+                  <div className="exp-month">Exp Month</div>
+                  <select>
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                    <option>4</option>
+                    <option>5</option>
+                    <option>6</option>
+                    <option>7</option>
+                    <option>8</option>
+                    <option>9</option>
+                    <option>10</option>
+                    <option>11</option>
+                    <option>12</option>
+                  </select>
+                </div>
 
-                <div>Exp Year</div>
-                <select>
-                  <option>2020</option>
-                  <option>2021</option>
-                  <option>2022</option>
-                  <option>2023</option>
-                  <option>2024</option>
-                </select>
+                <div className="exp-year-container">
+                  <div className="exp-year">Exp Year</div>
+                  <select>
+                    <option>2020</option>
+                    <option>2021</option>
+                    <option>2022</option>
+                    <option>2023</option>
+                    <option>2024</option>
+                  </select>
+                </div>
               </div>
 
-              <div>CVC</div>
+              <div className="label cvc">CVC</div>
               <input type="text"></input>
             </div>
+
             <div className="payment-summary">
               <div>qty: {this.props.seatsQty}</div>
               <div>seat: {this.props.selectedSeat}</div>
