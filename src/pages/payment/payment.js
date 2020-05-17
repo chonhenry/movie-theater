2bcffd5b59f83136c185c617b6c099cf1e1ae411
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-05-16 23:31:59 -0400

    added validation to credit card form on payment page

--- src/pages/payment/payment.js
+++ src/pages/payment/payment.js
@@ -3,7 +3,70 @@ import { connect } from "react-redux";
 import { fetchMovieDetail } from "../../actions/index";
 import "./payment.scss";
 
+// ae 34, 37
+// mc 51, 52, 53, 54, 55
+// visa 4
+// dis 6
+
 class Payment extends React.Component {
+  state = { type: "" };
+
+  onCreditCardNumChange = (e) => {
+    if (e.target.value.slice(-1) >= "0" && e.target.value.slice(-1) <= "9") {
+      e.target.value = e.target.value;
+    } else {
+      e.target.value = e.target.value.slice(0, e.target.value.length - 1);
+    }
+
+    if (e.target.value.slice(0, 1) === "4") {
+      this.setState({ type: "visa" });
+    } else if (e.target.value.slice(0, 1) === "6") {
+      this.setState({ type: "discover" });
+    } else if (
+      e.target.value.slice(0, 2) === "51" ||
+      e.target.value.slice(0, 2) === "52" ||
+      e.target.value.slice(0, 2) === "53" ||
+      e.target.value.slice(0, 2) === "54" ||
+      e.target.value.slice(0, 2) === "55"
+    ) {
+      this.setState({ type: "master" });
+    } else if (
+      e.target.value.slice(0, 2) === "34" ||
+      e.target.value.slice(0, 2) === "37"
+    ) {
+      this.setState({ type: "amex" });
+    } else {
+      this.setState({ type: "" });
+    }
+  };
+
+  showType = () => {
+    return (
+      <div className="cc-type-container">
+        <i
+          className={`cc-type fab fa-cc-visa fa-3x ${
+            this.state.type === "visa" ? "selected" : ""
+          }`}
+        ></i>
+        <i
+          className={`cc-type fab fa-cc-mastercard fa-3x ${
+            this.state.type === "master" ? "selected" : ""
+          }`}
+        ></i>
+        <i
+          className={`cc-type fab fa-cc-amex fa-3x ${
+            this.state.type === "amex" ? "selected" : ""
+          }`}
+        ></i>
+        <i
+          className={`cc-type fab fa-cc-discover fa-3x ${
+            this.state.type === "discover" ? "selected" : ""
+          }`}
+        ></i>
+      </div>
+    );
+  };
+
   render() {
     return (
       <div className="payment">
@@ -12,13 +75,16 @@ class Payment extends React.Component {
           <div className="payment-detail">
             <div className="credit-card-form">
               <div className="payment-label">Enter you payment details</div>
-
+              {this.showType()}
               <div className="label">Cardholder Name</div>
               <input type="text" className="cardholder-name"></input>
-
               <div className="label">Card Number</div>
-              <input type="text" className="card-number"></input>
-
+              <input
+                type="text"
+                onChange={(e) => this.onCreditCardNumChange(e)}
+                className="card-number"
+                maxLength={this.state.type === "amex" ? 15 : 16}
+              ></input>
               <div className="expiration">
                 <div className="exp-month-container">
                   <div className="exp-month">Exp Month</div>
@@ -51,9 +117,12 @@ class Payment extends React.Component {
                   </select>
                 </div>
               </div>
-
               <div className="label">CVC</div>
-              <input type="text" className="cvc"></input>
+              <input
+                type="text"
+                className="cvc"
+                maxLength={this.state.type === "amex" ? 4 : 3}
+              ></input>
             </div>
 
             <div className="payment-summary">
@@ -87,7 +156,7 @@ const mapStateToProps = (state) => {
   return {
     title: state.movieDetail.title,
     seatsQty: state.seatsInfo.seats_qty,
-    selectedSeat: state.seatsInfo.selectedSeat,
+    selectedSeat: state.seatsInfo.selectedSeat.sort(),
     date: state.date,
     time: state.time,
   };
