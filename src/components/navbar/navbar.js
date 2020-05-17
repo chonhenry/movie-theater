a0c0bf5b4fa3e6b5932cc73511fb33c12050da2d
Henry Chon <47991972+chonhenry@users.noreply.github.com>
2020-05-17 11:19:16 -0400

    add backdrop toggle on navbar search button

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,50 +1,60 @@
 import React from "react";
 import "./navbar.scss";
-import ToggleButton from "../toggle-button/toggle-button";
+// import ToggleButton from "../toggle-button/toggle-button";
+import Backdrop from "../backdrop/backdrop";
 import logo from "../../assets/images/logo.png";
 import { Link } from "react-router-dom";
 
-const Navbar = (props) => {
-  return (
-    <header className="navbar">
-      <nav className="navbar-navigation container">
-        <div className="toogle-btn">
-          <ToggleButton click={props.toggleClickHandler} />
-        </div>
-        <div className="navbar-logo">
-          <Link to="/" className="logo">
-            <img src={logo} alt="logo"></img>
-          </Link>
-        </div>
-        <div className="spacer"></div>
-        <div className="navbar-items">
-          <ul>
-            <li>
-              <Link to="/" className="link">
-                HOME
-              </Link>
-            </li>
-            <li>
-              <Link to="/location" className="link">
-                THEATERS
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="link">
-                SEARCH
-              </Link>
-            </li>
-            <li>
-              <Link to="/" className="link">
-                SIGNIN
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </header>
-  );
-};
+class Navbar extends React.Component {
+  state = { backdrop: false };
+
+  backdropToggle = (prevState) => {
+    this.setState({ backdrop: !prevState });
+  };
+
+  render() {
+    return (
+      <header className="navbar">
+        <nav className="navbar-navigation container">
+          {/* <div className="toogle-btn">
+            <ToggleButton click={props.toggleClickHandler} />
+          </div> */}
+          <div className="navbar-logo">
+            <Link to="/" className="logo">
+              <img src={logo} alt="logo"></img>
+            </Link>
+          </div>
+          <div className="spacer"></div>
+          <div className="navbar-items">
+            <ul>
+              {/* <li>
+                <Link to="/" className="link">
+                  HOME
+                </Link>
+              </li>
+              <li>
+                <Link to="/location" className="link">
+                  THEATERS
+                </Link>
+              </li>
+              <li>
+                <Link to="/" className="link">
+                  SIGNIN
+                </Link>
+              </li> */}
+              <li onClick={() => this.backdropToggle()}>
+                <Link to="/" className="link">
+                  <i className="fas fa-search fa-2x"></i>
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        {this.state.backdrop ? <Backdrop click={this.backdropToggle} /> : null}
+      </header>
+    );
+  }
+}
 
 export default Navbar;
 
